91cf04ff11d783678f1cc2faa892fa747a2ff1da
JnJuste <jeanjusteirakoze@gmail.com>
2023-11-07 10:55:14 +0200

    Enabling Student Course Model

--- src/main/java/com/jeanjuste/springmid24229/controllers/StudentCourseController.java
+++ src/main/java/com/jeanjuste/springmid24229/controllers/StudentCourseController.java
@@ -1,7 +1,12 @@
 package com.jeanjuste.springmid24229.controllers;
 
+import com.jeanjuste.springmid24229.models.Course;
 import com.jeanjuste.springmid24229.models.StudentCourse;
+import com.jeanjuste.springmid24229.models.StudentRegistration;
+import com.jeanjuste.springmid24229.services.CourseService;
 import com.jeanjuste.springmid24229.services.StudentCourseService;
+import com.jeanjuste.springmid24229.services.StudentRegistrationService;
+import com.jeanjuste.springmid24229.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,18 +19,28 @@ import java.util.List;
 @Controller
 public class StudentCourseController {
     private StudentCourseService studentCourseService;
+    private CourseService courseService;
+    private StudentRegistrationService studentRegistrationService;
+
 
     @Autowired
-    public StudentCourseController(StudentCourseService studentCourseService) {
+    public StudentCourseController(StudentCourseService studentCourseService, CourseService courseService,
+                                   StudentRegistrationService studentRegistrationService) {
         this.studentCourseService = studentCourseService;
+        this.studentRegistrationService = studentRegistrationService;
+        this.courseService = courseService;
     }
 
     @GetMapping("/studentCourses")
     public String createStudentCourseForm(Model model){
         StudentCourse studentCourse = new StudentCourse();
         List<StudentCourse> studentCourses = studentCourseService.findAllStudentCourses();
+        List<StudentRegistration> studentRegistrations = studentRegistrationService.findAllStudentRegistrations();
+        List<Course> courses = courseService.findAllCourses();
         model.addAttribute("studentCourses", studentCourses);
         model.addAttribute("studentCourse", studentCourse);
+        model.addAttribute("studentRegistrations", studentRegistrations);
+        model.addAttribute("courses", courses);
         return "StudentCourse";
     }
 
