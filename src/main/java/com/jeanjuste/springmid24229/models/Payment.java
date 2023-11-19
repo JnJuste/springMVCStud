6170ca2ae638127353cf926a01dc8cfaddffa593
JnJuste <jeanjusteirakoze@gmail.com>
2023-11-19 15:21:28 +0200

    Commit changes for Payment

---
+++ src/main/java/com/jeanjuste/springmid24229/models/Payment.java
@@ -0,0 +1,26 @@
+package com.jeanjuste.springmid24229.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "payment")
+public class Payment {
+    @Id
+    private UUID paymentID;
+    @OneToOne
+    private StudentRegistration studentRegistration;
+    private LocalDate paymentDate;
+    private BigDecimal amount;
+}
